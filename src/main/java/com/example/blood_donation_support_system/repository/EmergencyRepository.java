171c82873f97a693dc01cc9edfc2e1b458328d77
BryanNguyen <tinnguyen100904@gmail.com>
2025-06-23 15:10:21 +0700

    feat: finish manage-blood-donation-flow

--- src/main/java/com/example/blood_donation_support_system/repository/EmergencyRepository.java
+++ src/main/java/com/example/blood_donation_support_system/repository/EmergencyRepository.java
@@ -3,6 +3,7 @@ package com.example.blood_donation_support_system.repository;
 import com.example.blood_donation_support_system.entity.EmergencyEntity;
 import com.example.blood_donation_support_system.entity.UserEntity;
 import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface EmergencyRepository extends JpaRepository<EmergencyEntity, Integer> {
-    Optional<EmergencyEntity> findTopByRequesterOrderByCreatedAtDesc(UserEntity requester);
+    Optional<EmergencyEntity> findTopByRequesterAndStatusOrderByCreatedAtDesc(UserEntity requester, String status);
 }
